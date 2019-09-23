98c9d05252e7659154636f17deebd7f78e6e37b8
unknown <oghahroodi98@gmail.com>
2019-09-23 03:16:29 -0700

    Add script for count words

---
+++ word_count.py
@@ -0,0 +1,26 @@
+import glob
+import operator
+
+data_set = '..\\..\\org_data_set\\'
+
+
+text_file = glob.glob(data_set+'\\*\\*.txt')
+count = {}
+
+# print(text_file)
+for i in text_file:
+    f = open(i,'r+')
+    # print(f.read().split('\n'))
+    l = f.read().split('\n')
+    
+    for j in range(4,len(l)-1):
+        word = l[j].split()[0]
+        if word not in count.keys():
+            count[word]=1
+        else:
+            count[word]+=1
+    # break
+
+# print(count)
+sorted_count = sorted(count.items(), key=operator.itemgetter(1), reverse=True)
+print(sorted_count)
